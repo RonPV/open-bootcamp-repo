95284a06c8cdd5b180c55eef229151e30c2b1dff
Ron <you@example.com>
2023-03-17 22:27:39 -0500

    pruebas en el lenguaje dart

---
+++ dart/01-/variables/bin/variables.dart
@@ -0,0 +1,8 @@
+import 'package:variables/variables.dart' as variables;
+
+void main(List<String> arguments) {
+  var nombre = "Luis";
+  var edad = 29;
+  print("Mi nombre es: " + nombre);
+  print(edad.runtimeType);
+}
